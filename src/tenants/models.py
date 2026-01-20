903bb01934ee1ffeea089ecee7736f13616484bb
fahadAziz-byte <sp23-bse-089@cuilahore.edu.pk>
2026-01-20 10:19:26 +0500

    Making changes in tenant models and making post save signal in tenant model to commit migrations only after tenant object is created

--- src/tenants/models.py
+++ src/tenants/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 from .utils import generate_schema_name
 from helpers.db.validators import validate_blocked_subdomains,validate_subdomain
 from django.core.management import call_command
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import transaction
 import uuid
 from .tasks import migrate_single_tenant_task
 User=settings.AUTH_USER_MODEL
