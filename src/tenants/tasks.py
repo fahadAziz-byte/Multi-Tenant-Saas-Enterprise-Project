99c18cf76acacac97956e66c5e781950b98e7e6a
fahadAziz-byte <sp23-bse-089@cuilahore.edu.pk>
2026-03-23 16:43:49 +0500

    removed tenant duplication by adding try catch block in tenant middleware

--- src/tenants/tasks.py
+++ src/tenants/tasks.py
@@ -8,8 +8,8 @@ def migrate_single_tenant_task(tenant_id: str):
     instance = Tenants.objects.get(id=tenant_id)
     schema_name = instance.schema_name
 
-    with use_tenant_schema(schema_name=schema_name, create_if_missing=True):
-        try:
+    try:
+        with use_tenant_schema(schema_name=schema_name, create_if_missing=True):
             print(f"--- Running FULL Migration for schema: {schema_name} ---")
             
             # RUN ALL MIGRATIONS IN ONE SHOT
@@ -17,8 +17,10 @@ def migrate_single_tenant_task(tenant_id: str):
             call_command("migrate", interactive=False, verbosity=1)
             
             print(f"--- Successfully finished {schema_name} ---")
-        except Exception as e:
-            print(f"Error during migration: {e}")
-        finally:
-            # Crucial: Clean up connection to avoid "another command in progress" errors
-            connection.close() 
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        print(f"Error during migration: {e}")
+    finally:
+        # Crucial: Clean up connection to avoid "another command in progress" errors
+        connection.close() 
